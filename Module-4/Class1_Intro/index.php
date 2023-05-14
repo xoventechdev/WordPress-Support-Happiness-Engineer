817f020d666e5afdb36ca163c57724f17efa1fb2
xoventechdev <alkarim.doha@gmail.com>
2023-05-14 17:06:23 +0300

    m4 going

---
+++ Module-4/Class1_Intro/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>Intro of PHP</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col h-250 bg-primary p-5 text-white text-center">
+                <h1>মডিউল ৪: প্রোগ্রামিং ল্যাঙ্গুয়েজ (পিএইচপি)</h1>
+                <p class="p-3">পিএইচপি বেসিক – ইন্ট্রোডাকশন টু পিএইচপি এবং কিভাবে পিএইচপি কোড লিখে – পর্ব ১ ও ২</p>
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col code-box py-5 text-white">
+                <div class="input">
+                    <h2>PHP code</h2>
+                    <textarea class="w-100 p-3 bg-white text-dark fs-4" name="codearea" id="codearea" cols="30" rows="5">echo 'Hello, I'm practicing class 1 from PHP module.';</textarea>
+                </div>
+                <div class="output">
+                    <h2>Result</h2>
+                    <p class="w-100 p-3 bg-white text-dark  fs-4">
+
+                    <?php
+
+
+                        echo 'Hello, I\'m practicing class 1 from PHP module.';
+
+
+                    ?>   
+
+                    </p>
+
+                </div>
+            </div>
+        </div>
+        <footer class="row">
+            <div class="col bg-primary p-3 d-flex flex-column align-items-center text-white">
+                <h4>Web developed by XovenTech</h4>
+                <p>Copyrights © 2023</p>
+            </div>
+        </footer>
+    </div>
+
+
+
+</body>
+</html>
