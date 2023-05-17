c0c3e034284721a30c7ab3b82a99047223e5b602
xoventechdev <alkarim.doha@gmail.com>
2023-05-17 10:09:52 +0300

    m4 c7 done

---
+++ Module-4/Class7_PHP Arrays/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>PHP Arrays</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col h-250 bg-primary p-5 text-white text-center">
+                <h1>মডিউল ৪: প্রোগ্রামিং ল্যাঙ্গুয়েজ (পিএইচপি)</h1>
+                <p class="p-3">পিএইচপি বেসিক – অ্যারে – পর্ব ৭</p>
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col code-box py-5 text-white">
+                <div class="input">
+                    <h2>PHP code</h2>
+                    <textarea class="w-100 p-3 bg-white text-dark fs-4" name="codearea" id="codearea" cols="30" rows="10">
+                    $jobs = ['Web Developer', 'App Developer', 60, false];
+
+
+                    if($jobs[0] == 'Web Developer'){
+                    echo 'Hello, I\'m a '.$jobs[0] . '. I also know '. $jobs[1].'.  I work more than '.$jobs[2].' hours in a week.<br>';
+                    } else {
+                        echo 'Hello, I\'m a Android Developer. <br>';
+                    }
+
+                    if($jobs[3]){
+                        echo 'It\'s true that....... <br>';
+                    } else {
+                        echo 'It\'s fasle that....... <br>';
+                    }
+                    </textarea>
+                </div>
+                <div class="output">
+                    <h2>Result</h2>
+                    <p class="w-100 p-3 bg-white text-dark  fs-4">
+
+                    <?php
+
+
+
+                        $jobs = ['Web Developer', 'App Developer', 60, false];
+
+                        echo ' $jobs = [\'Web Developer\', \'App Developer\', 60, false]; <br><br>';
+
+                        if($jobs[0] == 'Web Developer'){
+                           echo 'Hello, I\'m a '.$jobs[0] . '. I also know '. $jobs[1].'.  I work more than '.$jobs[2].' hours in a week.<br>';
+                        } else {
+                            echo 'Hello, I\'m a Android Developer. <br>';
+                        }
+
+                        if($jobs[3]){
+                            echo 'It\'s true that....... <br>';
+                        } else {
+                            echo 'It\'s fasle that....... <br>';
+                        }
+
+                
+
+                    ?>   
+
+                    </p>
+
+                </div>
+            </div>
+        </div>
+        <footer class="row">
+            <div class="col bg-primary p-3 d-flex flex-column align-items-center text-white">
+                <h4>Web developed by XovenTech</h4>
+                <p>Copyrights © 2023</p>
+            </div>
+        </footer>
+    </div>
+
+
+
+</body>
+</html>
