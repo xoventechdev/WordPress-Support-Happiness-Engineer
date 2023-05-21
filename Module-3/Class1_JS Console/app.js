9f3df06f02fe6b5e8fde2ceb7ec04e4aec553074
xoventechdev <alkarim.doha@gmail.com>
2023-05-21 10:12:00 +0300

    m3 c1 and c2 added

---
+++ Module-3/Class1_JS Console/app.js
@@ -0,0 +1,9 @@
+
+
+console.log('This is a log.');
+console.log(20);
+console.log(12+3);
+
+
+
+alert('This is a alert');
---
+++ Module-3/Class2_Variables and Datatypes/app.js
@@ -0,0 +1,35 @@
+
+//integer
+var a = 12;
+console.log(a);
+console.log(typeof(a));
+
+//string
+var b = 'This is a string type data.'
+console.log(b);
+console.log(typeof(b));
+
+//boolean
+var c = true;
+console.log(c);
+console.log(typeof(c));
+
+//null
+var d = null;
+console.log(d);
+console.log(typeof(d));
+
+//undefined
+var e;
+console.log(e);
+console.log(typeof(e));
+
+
+//single variable
+var name;
+
+//multiple variable
+var fName, lName, age;
+
+//variable with value
+var age = 13;
