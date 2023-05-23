a6300c3687ede6b3332e7b6f07be131df0ee95d3
xoventechdev <alkarim.doha@gmail.com>
2023-05-23 12:31:06 +0300

    m3 c3 done

--- Module-3/Class3_JavaScript Operators/app.js
+++ Module-3/Class3_JavaScript Operators/app.js
@@ -19,3 +19,26 @@ console.group(a);
 
 //Arithmetic Operators
 console.log('Arithmetic Operators');
+var b = 2;
+var c = 3;
+console.log(b+c);
+console.log(++b);
+console.log(c++);
+console.log(c++);
+console.log(--c);
+
+//Comparison Operators
+console.log('Comparison Operators');
+var e = 4, f = 4, g = 5, h = '5';
+console.log(e == f);
+console.log(f == g);
+console.log(f != g);
+console.log(g == h);
+console.log(g === h);
+console.log(f > g);
+console.log(e <= f);
+
+//Bitwise Operators
+console.log('Bitwise Operators');
+var k = 2, j = 3, m = true, n = false;
+console.log((k == j) == n);
