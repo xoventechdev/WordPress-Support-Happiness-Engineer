fff9d81ce9587ce9b6453d0bf2de97126ca5c146
xoventechdev <alkarim.doha@gmail.com>
2023-05-23 11:15:36 +0300

    test

---
+++ Module-3/Class3_JavaScript Operators/app.js
@@ -0,0 +1,21 @@
+
+
+//Assignment Operators
+console.log('Assignment Operators');
+var a = 2;
+console.log(a);
+a += 3;
+console.log(a);
+a -= 1;
+console.log(a);
+a *= 3;
+console.log(a)
+a /= 2;
+console.log(a);
+a %= 4;
+console.log(a);
+a **= 2;
+console.group(a);
+
+//Arithmetic Operators
+console.log('Arithmetic Operators');
