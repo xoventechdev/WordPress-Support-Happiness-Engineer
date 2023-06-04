6a4eb87b5a06e74b850852d2aa3012f2f749a325
xoventechdev <alkarim.doha@gmail.com>
2023-06-04 14:33:02 +0300

    m3 c8 added

---
+++ Module-3/Class8_JavaScript Objects/app.js
@@ -0,0 +1,37 @@
+
+
+
+
+var worker = {
+    id : 1,
+    name : 'Saif Ali',
+    position : 'Manager',
+    salary : 1200,
+    city : 'Dhaka'
+}
+
+console.log('Mr. ' + worker.name + ' work at Stark LLC as a ' +worker.position +'. He get every month ' + worker.salary + '. He living in ' + worker.city);
+
+
+var company = new Object();
+company.id = 1;
+company.name = 'Stark LLC';
+company.worker = 100;
+company.office = 'Dhaka';
+
+console.log(company.name + ' is one of the best company in security sector. There are total '+ company.worker+ ' security person work there. All of them living in comapny camp in '+company.office);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById("code_console").innerHTML  = toConsoleFor +"<br/>"+ toConsoleDoWhile +"<br/>"+ toConsoleWhile;
